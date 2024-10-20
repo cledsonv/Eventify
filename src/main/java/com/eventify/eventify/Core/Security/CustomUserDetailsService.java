789f52e7ffd044b65e81356312a7eb191335b1e5
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2024-10-19 21:08:19 -0300

    feat security

---
+++ src/main/java/com/eventify/eventify/Core/Security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.eventify.eventify.Core.Security;
+
+import com.eventify.eventify.Features.User.Entities.User;
+import com.eventify.eventify.Features.User.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), new ArrayList<>());
+    }
+}
