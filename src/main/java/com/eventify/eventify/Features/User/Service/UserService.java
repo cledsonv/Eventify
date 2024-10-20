9c65316eac40fe70912afb213b05d620c6779117
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2024-10-19 21:08:40 -0300

    feat User

---
+++ src/main/java/com/eventify/eventify/Features/User/Service/UserService.java
@@ -0,0 +1,30 @@
+package com.eventify.eventify.Features.User.Service;
+
+import com.eventify.eventify.Features.User.Entities.User;
+import com.eventify.eventify.Features.User.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUser(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
